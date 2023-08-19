9dd0458f83b1f3e62446b6850244f774c83c2dee
Adam Abdillah <adamabdillah20@gmail.com>
2023-08-20 01:39:29 +0700

    fix: fixing component on navbar

--- components/organism/navbar/index.vue
+++ components/organism/navbar/index.vue
@@ -18,7 +18,7 @@
         class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
       >
         <ul
-          class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white"
+          class="flex gap-4 flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white"
         >
           <MoleculesNavbarLink
             v-for="(item, index) in response.data"
