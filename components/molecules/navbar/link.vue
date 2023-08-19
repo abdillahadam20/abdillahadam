4ba4101b2a702765705e6d44776b97f2e758a9d7
Adam Abdillah <adamabdillah20@gmail.com>
2023-08-20 01:09:06 +0700

    feat(component based): adding component on navbar

---
+++ components/molecules/navbar/link.vue
@@ -0,0 +1,29 @@
+<template>
+  <li>
+    <nuxt-link :to="data.href">
+      <span class="title-link">
+        {{ data.title }}
+      </span>
+    </nuxt-link>
+  </li>
+</template>
+
+<script setup lang="ts">
+interface Link {
+  title: string
+  href: string
+}
+
+defineProps({
+  data: {
+    type: Object as PropType<Link>,
+    default: () => ({}) as Link,
+  },
+})
+</script>
+
+<style scoped lang="postcss">
+span {
+  @apply text-base-tasty-500 font-bold text-base font-poppins;
+}
+</style>
---
+++ server/api/navbar.get.ts
@@ -0,0 +1,22 @@
+export default defineEventHandler(() => {
+  return {
+    data: [
+      {
+        title: 'Home',
+        href: '/',
+      },
+      {
+        title: 'About-Us',
+        href: '/',
+      },
+      {
+        title: 'Experience',
+        href: '/',
+      },
+      {
+        title: 'Contact-Us',
+        href: '/',
+      },
+    ],
+  }
+})
