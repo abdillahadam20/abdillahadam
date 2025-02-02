3a7868bb7ac654cadebce98f4fcc170792a05a1b
Adam Abdillah <adamabdillah20@gmail.com>
2025-02-02 14:30:40 +0700

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="postcss" scoped>
+.app {
+  @apply font-poppins;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,38 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  css: ["~/assets/css/tailwind.css"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxtjs/color-mode",
+    "@nuxt/fonts",
+  ],
+  postcss: {
+    plugins: {
+      "tailwindcss/nested": {},
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+  fonts: {
+    families: [{ name: "Poppins", provider: "google" }],
+    defaults: {
+      weights: [500, 600, 700, 800],
+      styles: ["normal"],
+      subsets: ["latin"],
+    },
+  },
+});
