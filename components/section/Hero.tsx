1f7eb147de073ebcc669993f9316312029d5e5b6
Adam Abdillah <adamabdillah20@gmail.com>
2025-06-16 12:20:07 +0700

    fixing url cv in hero

--- components/section/Hero.tsx
+++ components/section/Hero.tsx
@@ -99,7 +99,7 @@ const Hero = () => {
                 transition={{ duration: 1, delay: 0.7 }}
               >
                 <Link
-                  href="https://drive.google.com/file/d/1WbuzgbuaT2UYnvRBJYGvXgIxrcR53dga/view?usp=sharing"
+                  href="https://drive.google.com/file/d/1xohQNy2lsGafycvwDyk_r_TgFXqxkg8y/view?usp=sharing"
                   target="_blank"
                 >
                   <Button className="cursor-pointer" variant="emerald">
